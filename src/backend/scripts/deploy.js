51f37fa9ea1b32ad947614311ef7dc612746d7d3
fatihemreyildiz <fatihemreyildiz@gmail.com>
2024-01-14 04:24:00 +0300

    Deploy.JS & Marketplace.sol: added

--- src/backend/scripts/deploy.js
+++ src/backend/scripts/deploy.js
@@ -1,34 +1,53 @@
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const [deployer] = await hre.ethers.getSigners();
 
-  const lockedAmount = hre.ethers.parseEther("0.001");
+  console.log("Deploying contracts with the account:", deployer.address);
+  console.log("Account balance:", (await deployer.provider.getBalance(deployer.address)).toString());
 
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
-
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
-
-  const NFT = await ethers.getContractFactory("NFT");
+  const NFT = await hre.ethers.getContractFactory("NFT");
+  const Marketplace = await hre.ethers.getContractFactory("Marketplace");
+  
   const nft = await NFT.deploy();
+  const marketplace = await Marketplace.deploy(1);
+  
+  console.log("Deploying contracts with the account:", deployer.address);
+  console.log("MarketPlace contract is on the: ", (marketplace.address));
+  console.log("NFT Contract Address is on the: ", (nft.address));
 
-  console.log(
-    `NFT unlock timestamp ${unlockTime} deployed to ${nft.address}`
-  );
+  // console.log("Account balance after the deployment:", (await deployer.provider.getBalance(deployer.address)).toString());
+  // console.log("Account balance after the deployment of marketplace:", (await marketplace.provider.getBalance(marketplace.address)).toString());
+  // console.log("Account balance after the deployment of nft:", (await nft.provider.getBalance(nft.address)).toString());
 
-  // console.log("NFT deployed to:", nft.address);
+  saveFrontendFiles(nft , "NFT");
+  saveFrontendFiles(marketplace , "Marketplace");
 }
 
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+function saveFrontendFiles(contract, name) {
+  const fs = require("fs");
+  const contractsDir = __dirname + "/../../PTR_NFT_Front/src/contractsData";
+
+  if (!fs.existsSync(contractsDir)) {
+    fs.mkdirSync(contractsDir);
+  }
+
+  fs.writeFileSync(
+    contractsDir + `/${name}-address.json`,
+    JSON.stringify({ address: contract.address }, undefined, 2)
+  );
+
+  const contractArtifact = artifacts.readArtifactSync(name);
+
+  fs.writeFileSync(
+    contractsDir + `/${name}.json`,
+    JSON.stringify(contractArtifact, null, 2)
+  );
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
