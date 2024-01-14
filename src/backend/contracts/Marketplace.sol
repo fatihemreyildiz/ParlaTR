21b67aa869d5a5e343a9dc83ffff364692905a1f
Halim Ceylan <halimceylan7@gmail.com>
2024-01-15 00:44:49 +0300

    purchase update

--- src/backend/contracts/Marketplace.sol
+++ src/backend/contracts/Marketplace.sol
@@ -71,16 +71,14 @@ contract Marketplace is ReentrancyGuard {
         uint _totalPrice = getTotalPrice(_itemId);
         Item storage item = items[_itemId];
         require(_itemId > 0 && _itemId <= itemCount, "item doesn't exist");
-        require(msg.value >= _totalPrice, "not enough ether to cover item price and market fee");
+        require(msg.sender.balance >= _totalPrice, "not enough Ptr_nft to cover item price and market fee");
         require(!item.sold, "item already sold");
 
-        item.seller.transfer(item.price);
-        feeAccount.transfer(_totalPrice - item.price);
-
+        (bool success, ) = item.seller.call{value:item.price}("");
+        require(success, "Transfer failed.");
         item.sold = true;
 
         item.nft.transferFrom(address(this), msg.sender, item.tokenId);
-
         emit Bought(
             _itemId,
             address(item.nft),
